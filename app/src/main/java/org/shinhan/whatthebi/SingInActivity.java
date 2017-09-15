de70e69b4403f68f8e5bc2a2220050bd7a12a331
Young <babjul314@ajou.ac.kr>
2017-09-15 17:20:46 +0900

    디자인 버전1

---
+++ app/src/main/java/org/shinhan/whatthebi/SingInActivity.java
@@ -0,0 +1,18 @@
+package org.shinhan.whatthebi;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 60029509 on 2017-09-15.
+ */
+
+public class SingInActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
---
+++ app/src/main/java/org/shinhan/whatthebi/WhereActivity.java
@@ -0,0 +1,17 @@
+package org.shinhan.whatthebi;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 60029509 on 2017-09-15.
+ */
+
+public class WhereActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
